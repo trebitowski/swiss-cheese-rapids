de331fed823477b980b5c8125e911148423b14c3
trebitowski <tyler.trebitowski@gmail.com>
2022-12-18 15:29:40 -0600

    Score and game over

---
+++ Assets/GameManager.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+    public void EndGame() {
+        if (gameHasEnded == false) {
+            gameHasEnded = true;
+            Debug.Log("Game Over!");
+        }
+    }
+}
