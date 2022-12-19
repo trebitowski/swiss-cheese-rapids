e02ad851bea329da1cb4815ac2f43f3e46f6fe23
sequoyah <sequoyah.n.walters@gmail.com>
2022-12-19 10:39:32 -0600

    white caps

--- Assets/Scripts/RaftScript.cs
+++ Assets/Scripts/RaftScript.cs
@@ -20,7 +20,7 @@ public class RaftScript : MonoBehaviour
     public float returnAngleStrength;
     public float targetAngle = 0;
     public float riverSpeed = 8f;
-    public float rapidSpeed = 12f;
+    public static float rapidSpeed = 12f;
     // public float currentSpeed;
     float rotateBack = 0f;
     LayerMask riverMask;
